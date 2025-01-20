8808d3ab82a78c379a2d026647474f35cb2217c6
rishi998 <mehtorishi62203@gmail.com>
2025-01-20 13:07:59 +0530

    done

--- app.py
+++ app.py
@@ -1,3 +1,51 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:557619cf71f7715e3a034c05a9c01e954140e768ca6e7725396cf1c98888bfc4
-size 1686
+import pickle
+import pandas as pd
+import streamlit as st
+import requests
+
+st.title('Movie Recommender System')
+
+movies_dict=pickle.load(open('movie_dict.pkl','rb'))
+movies=pd.DataFrame(movies_dict)
+similarity=pickle.load(open('similarity.pkl','rb'))
+
+def fetch_poster(movie_id):
+    response=requests.get("http://api.themoviedb.org/3/movie/{}?api_key=a6c7860d1a82a2855a800b32e770fbcd".format(movie_id))
+    data=response.json()
+    print(data)
+    return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)),reverse=True, key=lambda x:x[1])[1:6]
+    recommended_movies = []
+    recommended_movies_posters=[]
+    for i in movie_list:
+        movie_id=movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
+
+
+selected_movie_name=st.selectbox("Type in your Movie!",movies['title'].values)
+
+if st.button('Recommend'):
+    names,posters=recommend(selected_movie_name)
+    col1, col2,col3,col4,col5=st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
